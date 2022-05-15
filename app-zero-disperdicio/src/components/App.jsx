3027cbd5ddd8a4195c9789533df55e4d8e091a6b
Gilmar Santos <gneto1999@hotmail.com>
2022-05-15 16:35:53 -0300

    Adição do React no Projeto

---
+++ app-zero-disperdicio/src/components/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'; 
+import Header from './header/Header.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
