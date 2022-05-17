2397c43262b9eae18b6f16ef6e700d82c1890ddf
Gilmar Santos <gneto1999@hotmail.com>
2022-05-17 01:30:42 -0300

    Componentização do Header, Login, Menu de notificações e Alerta

--- app-zero-disperdicio/src/components/header/Header.jsx
+++ app-zero-disperdicio/src/components/header/Header.jsx
@@ -1,9 +1,17 @@
 import './Header.css'
+import UserActions from './UserActions'
+import NotificationsBell from './NotificationsBell'
 
-// function Header(){
-//     return (
-        
-//     )
-// }
+function Header(){
+    return (
+        <header id="header">
+            <div id="header-items" className="container">
+                <h1>Logo</h1>
+                <UserActions/>
+                <NotificationsBell/>
+            </div>
+        </header>
+    )
+}
 
 export default Header
