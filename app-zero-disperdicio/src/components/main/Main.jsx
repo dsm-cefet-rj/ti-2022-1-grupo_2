23be09ef3cf2d93b3b61c940991197667c648e49
Gabriel do Nascimento Alves <bielnascimento13@gmail.com>
2022-05-17 22:22:21 -0300

    form

--- app-zero-disperdicio/src/components/main/Main.jsx
+++ app-zero-disperdicio/src/components/main/Main.jsx
@@ -1,10 +1,12 @@
 import Alert from './Alert'
+import Form from './Form';
 import Accordion from './Accordion'
 import './Main.css'
 
 export default function Main(){
     return(
         <main className="container">
+            <Form/>
             <Accordion/>
             <Alert/>
         </main>
