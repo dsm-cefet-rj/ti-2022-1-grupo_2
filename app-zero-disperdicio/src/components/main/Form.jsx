06968718c4fed78adaa86310b97033f7f733c000
Gabriel do Nascimento Alves <bielnascimento13@gmail.com>
2022-05-24 10:51:23 -0300

    useState

--- app-zero-disperdicio/src/components/main/Form.jsx
+++ app-zero-disperdicio/src/components/main/Form.jsx
@@ -1,6 +1,23 @@
+import React, {useState} from 'react';
 import './Form.css';
 
 export default function Form(){
+    const [product, setProduct] = useState({});
+    const [validade, setValidade] = useState({});
+    const [quantidade, setQuantidade] = useState({});
+
+    function handleInputChange1(e){
+        setProduct( {...product, [e.target.value]:e.target.value} );
+    }
+
+    function handleInputChange2(e){
+        setValidade( {...validade, [e.target.value]:e.target.value} );
+    }
+
+    function handleInputChange3(e){
+        setQuantidade( {...quantidade, [e.target.value]:e.target.value} );
+    }
+
     return(
 <form action="#" id="register-product" method="post">
             <div id="category-container">
@@ -15,17 +32,17 @@ export default function Form(){
     
             <div id="name-container">
                 <label for="name-product">Nome do produto</label>
-                <input type="text" className="input" id="name-product" name="name-product" placeholder="Nome do produto"/>
+                <input type="text" className="input" id="name-product" name="name" value={product.name} placeholder="Nome do produto" onChange={handleInputChange1} />
             </div>
     
             <div id="expiration-container">
                 <label for="expiration-date">Data de validade</label>
-                <input type="date" className="input" id="expiration-date" name="expirations-date"/>
+                <input type="date" className="input" id="expiration-date" name="expirations" value={validade.expirations} onChange={handleInputChange2} />
             </div>
     
             <div id="quantity-container">
                 <label for="quantity">Quantidade</label>
-                <input type="text" className="input" id="quantity" name="quantity" placeholder="Quantidade"/>
+                <input type="text" className="input" id="quantity" name="quantity" placeholder="Quantidade" value={quantidade.quantity} onChange={handleInputChange3} />
             </div>
     
             <div id="comments-container">
