c760a0658ea7d863fb13c22561ebba69d086b68d
Gabriel do Nascimento Alves <bielnascimento13@gmail.com>
2022-07-17 20:44:02 -0300

    Ignorar

--- app-zero-disperdicio/src/TiposProdutos/TipoGeladeira.jsx
+++ app-zero-disperdicio/src/TiposProdutos/TipoGeladeira.jsx
@@ -1,7 +1,6 @@
 import React, {useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Tabee } from '../components/main/Table';
-import { fetchProjeto, selectAllProjeto } from '../comoponents/main/ProjetoSlice';
+import { fetchProjeto, selectAllProjeto } from '../components/main/ProjetoSlice';
 
 
 export default function ListagemGeladeira() {
